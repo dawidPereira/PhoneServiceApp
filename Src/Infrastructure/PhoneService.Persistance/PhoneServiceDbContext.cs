508106d719c3c88574385cf96fc308466447ab36
Dawid Pereira <pereiradawid@outlook.com>
2018-12-15 18:59:42 +0100

    Add Seed Data

--- Src/Infrastructure/PhoneService.Persistance/PhoneServiceDbContext.cs
+++ Src/Infrastructure/PhoneService.Persistance/PhoneServiceDbContext.cs
@@ -37,7 +37,7 @@ namespace PhoneService.Persistance
        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.ApplyAllConfigurations();
+            //modelBuilder.ApplyAllConfigurations();
 
             #region Setting entities relationships
 
@@ -80,6 +80,81 @@ namespace PhoneService.Persistance
                 .HasOne<SaparePart>(sp => sp.SaparePart)
                 .WithMany(psp => psp.RepairItems)
                 .HasForeignKey(sp => sp.SaparePartId);
+
+            #endregion
+
+            #region Seed Data
+
+            modelBuilder.Entity<Customer>().HasData(
+                new Customer { CustomerId = 1, Email = "pereiradawid@example.com", Name = "Dawid", LastName = "Pereira", PhoneNum = "123456789", TaxNum = "987654321" },
+                new Customer { CustomerId = 2, Email = "maksymilainmatula@example.com", Name = "Maksymilian", LastName = "Matuła", PhoneNum = "123456789", TaxNum = "987654321" },
+                new Customer { CustomerId = 3, Email = "miloszwinnicki@example.com", Name = "Miłosz", LastName = "Winnicki", PhoneNum = "123456789", TaxNum = "987654321" },
+                new Customer { CustomerId = 4, Email = "rafalpasek@example.com", Name = "Rafał", LastName = "Pasek", PhoneNum = "123456789", TaxNum = "987654321" },
+                new Customer { CustomerId = 5, Email = "grzegorzwojcik@example.com", Name = "Grzegorz", LastName = "Wójcik", PhoneNum = "123456789", TaxNum = "987654321" });
+
+            modelBuilder.Entity<CustomerAddres>().HasData(
+                new CustomerAddres { CustomerAddresId = 1, City = "Przeworsk", PostCode = "37-200", CustomerId = 1 },
+                new CustomerAddres { CustomerAddresId = 2, City = "Dąbrowa Górnicza", PostCode = "43-204", CustomerId = 2 },
+                new CustomerAddres { CustomerAddresId = 3, City = "Rzeszów", PostCode = "35-356", CustomerId = 3 },
+                new CustomerAddres { CustomerAddresId = 4, City = "Sosnowiec", PostCode = "30-300", CustomerId = 4 },
+                new CustomerAddres { CustomerAddresId = 5, City = "Żwirki i Muchomorki", PostCode = "11-222", CustomerId = 5 });
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product { ProductId = 1, Producer = "Samsung", Model = "S8", Description = "Nie wiem po co nam opis produktu" },
+                new Product { ProductId = 2, Producer = "Nokia", Model = "3310", Description = "Mam wrażenie, że trzeba usunąć opis" },
+                new Product { ProductId = 3, Producer = "Xiaomi", Model = "Lepszy", Description = "Przepraszam czy to pomyłka" },
+                new Product { ProductId = 4, Producer = "Apple", Model = "XXX", Description = "Policja? - Proszę przyjechać na facebooka" },
+                new Product { ProductId = 5, Producer = "Sony Ericson", Model = "Ericson Sony", Description = "Co jam ma tu wpisać ?" });
+
+            modelBuilder.Entity<SaparePart>().HasData(
+                new SaparePart { SaparePartId = 1, Name = "Dioda W34", Price = 10, },
+                new SaparePart { SaparePartId = 2, Name = "Tranzystor BX11", Price = 10, },
+                new SaparePart { SaparePartId = 3, Name = "Wyświetlacz uniwersalny", Price = 10, },
+                new SaparePart { SaparePartId = 4, Name = "O co to za śróbka", Price = 10, },
+                new SaparePart { SaparePartId = 5, Name = "Klawiatura 3310", Price = 10, });
+
+            modelBuilder.Entity<ProductSaparePart>().HasData(
+                new ProductSaparePart { ProductId = 1, SaparePartId = 1 },
+                new ProductSaparePart { ProductId = 1, SaparePartId = 3 },
+                new ProductSaparePart { ProductId = 2, SaparePartId = 5 },
+                new ProductSaparePart { ProductId = 2, SaparePartId = 2 },
+                new ProductSaparePart { ProductId = 3, SaparePartId = 3 },
+                new ProductSaparePart { ProductId = 3, SaparePartId = 4 },
+                new ProductSaparePart { ProductId = 4, SaparePartId = 1 },
+                new ProductSaparePart { ProductId = 4, SaparePartId = 2 },
+                new ProductSaparePart { ProductId = 5, SaparePartId = 4 },
+                new ProductSaparePart { ProductId = 5, SaparePartId = 3 });
+
+            modelBuilder.Entity<RepairStatus>().HasData(
+                new RepairStatus { RepairStatusId = 1, Name = "Przyjeta" },
+                new RepairStatus { RepairStatusId = 2, Name = "Wyceniona" },
+                new RepairStatus { RepairStatusId = 3, Name = "W trakcie realizacji" },
+                new RepairStatus { RepairStatusId = 4, Name = "Zrealizowana" },
+                new RepairStatus { RepairStatusId = 5, Name = "Odrzucona" },
+                new RepairStatus { RepairStatusId = 6, Name = "Będziesz Pan zadowolony" });
+
+            modelBuilder.Entity<Repair>().HasData(
+                new Repair { RepairId = 1, CustomerId = 1, ProductId = 1, RepairStatusId = 1, CreateDate = DateTime.UtcNow, Description = "Tutaj powinien być jakiś opis naprawy" },
+                new Repair { RepairId = 2, CustomerId = 1, ProductId = 5, RepairStatusId = 2, CreateDate = DateTime.UtcNow, Description = "Opis z produktu dodamu tutaj" },
+                new Repair { RepairId = 3, CustomerId = 2, ProductId = 2, RepairStatusId = 3, CreateDate = DateTime.UtcNow, Description = "Klient nie może dodzwonić się do nikogo - nie opłacił abonamentu" },
+                new Repair { RepairId = 4, CustomerId = 3, ProductId = 3, RepairStatusId = 4, CreateDate = DateTime.UtcNow, Description = "Klientowi nie działa klawiatura" },
+                new Repair { RepairId = 5, CustomerId = 3, ProductId = 4, RepairStatusId = 5, CreateDate = DateTime.UtcNow, Description = "Popsuty głośnik" },
+                new Repair { RepairId = 6, CustomerId = 4, ProductId = 1, RepairStatusId = 6, CreateDate = DateTime.UtcNow, Description = "Klient przyniusł zalany telefon w skarpecie z ryżem" },
+                new Repair { RepairId = 7, CustomerId = 5, ProductId = 2, RepairStatusId = 2, CreateDate = DateTime.UtcNow, Description = "Coś nie diała" },
+                new Repair { RepairId = 8, CustomerId = 5, ProductId = 5, RepairStatusId = 6, CreateDate = DateTime.UtcNow, Description = "Pan nie był zadowolony" });
+
+            modelBuilder.Entity<RepairItem>().HasData(
+                new RepairItem { RepairId = 1, SaparePartId = 1, Quantity = 1 },
+                new RepairItem { RepairId = 1, SaparePartId = 2, Quantity = 2 },
+                new RepairItem { RepairId = 2, SaparePartId = 3, Quantity = 1 },
+                new RepairItem { RepairId = 3, SaparePartId = 4, Quantity = 2 },
+                new RepairItem { RepairId = 4, SaparePartId = 5, Quantity = 1 },
+                new RepairItem { RepairId = 5, SaparePartId = 1, Quantity = 1 },
+                new RepairItem { RepairId = 2, SaparePartId = 2, Quantity = 2 },
+                new RepairItem { RepairId = 3, SaparePartId = 3, Quantity = 3 },
+                new RepairItem { RepairId = 4, SaparePartId = 4, Quantity = 1 },
+                new RepairItem { RepairId = 5, SaparePartId = 5, Quantity = 10 },
+                new RepairItem { RepairId = 2, SaparePartId = 1, Quantity = 1 });
 
             #endregion
         }
---
+++ Src/WebApp/PhoneService.App/PhoneService.App/Migrations/20181215175838_Add Seed Data.cs
@@ -0,0 +1,494 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PhoneService.App.Migrations
+{
+    public partial class AddSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SaparePartItems_Repairs_RepairId",
+                table: "SaparePartItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_SaparePartItems_SapareParts_SaparePartId",
+                table: "SaparePartItems");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SaparePartItems",
+                table: "SaparePartItems");
+
+            migrationBuilder.RenameTable(
+                name: "SaparePartItems",
+                newName: "RepairItems");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_SaparePartItems_SaparePartId",
+                table: "RepairItems",
+                newName: "IX_RepairItems_SaparePartId");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreateDate",
+                table: "Repairs",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RepairItems",
+                table: "RepairItems",
+                columns: new[] { "RepairId", "SaparePartId" });
+
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "CustomerId", "Email", "LastName", "Name", "PhoneNum", "TaxNum" },
+                values: new object[,]
+                {
+                    { 1, "pereiradawid@example.com", "Pereira", "Dawid", "123456789", "987654321" },
+                    { 2, "maksymilainmatula@example.com", "Matuła", "Maksymilian", "123456789", "987654321" },
+                    { 3, "miloszwinnicki@example.com", "Winnicki", "Miłosz", "123456789", "987654321" },
+                    { 4, "rafalpasek@example.com", "Pasek", "Rafał", "123456789", "987654321" },
+                    { 5, "grzegorzwojcik@example.com", "Wójcik", "Grzegorz", "123456789", "987654321" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "Description", "Model", "Producer" },
+                values: new object[,]
+                {
+                    { 1, "Nie wiem po co nam opis produktu", "S8", "Samsung" },
+                    { 2, "Mam wrażenie, że trzeba usunąć opis", "3310", "Nokia" },
+                    { 3, "Przepraszam czy to pomyłka", "Lepszy", "Xiaomi" },
+                    { 4, "Policja? - Proszę przyjechać na facebooka", "XXX", "Apple" },
+                    { 5, "Co jam ma tu wpisać ?", "Ericson Sony", "Sony Ericson" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "RepairStatuses",
+                columns: new[] { "RepairStatusId", "Name" },
+                values: new object[,]
+                {
+                    { 6, "Będziesz Pan zadowolony" },
+                    { 5, "Odrzucona" },
+                    { 4, "Zrealizowana" },
+                    { 1, "Przyjeta" },
+                    { 2, "Wyceniona" },
+                    { 3, "W trakcie realizacji" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "SapareParts",
+                columns: new[] { "SaparePartId", "Name", "Price", "ReferenceNumebr" },
+                values: new object[,]
+                {
+                    { 4, "O co to za śróbka", 10m, null },
+                    { 1, "Dioda W34", 10m, null },
+                    { 2, "Tranzystor BX11", 10m, null },
+                    { 3, "Wyświetlacz uniwersalny", 10m, null },
+                    { 5, "Klawiatura 3310", 10m, null }
+                });
+
+            migrationBuilder.InsertData(
+                table: "CustomerAddres",
+                columns: new[] { "CustomerAddresId", "Adress", "City", "CustomerId", "PostCode" },
+                values: new object[,]
+                {
+                    { 1, null, "Przeworsk", 1, "37-200" },
+                    { 2, null, "Dąbrowa Górnicza", 2, "43-204" },
+                    { 3, null, "Rzeszów", 3, "35-356" },
+                    { 4, null, "Sosnowiec", 4, "30-300" },
+                    { 5, null, "Żwirki i Muchomorki", 5, "11-222" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductSapareParts",
+                columns: new[] { "ProductId", "SaparePartId" },
+                values: new object[,]
+                {
+                    { 3, 4 },
+                    { 5, 3 },
+                    { 3, 3 },
+                    { 1, 3 },
+                    { 4, 2 },
+                    { 2, 2 },
+                    { 4, 1 },
+                    { 1, 1 },
+                    { 2, 5 },
+                    { 5, 4 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Repairs",
+                columns: new[] { "RepairId", "CreateDate", "CustomerId", "Description", "ProductId", "RepairStatusId" },
+                values: new object[,]
+                {
+                    { 5, new DateTime(2018, 12, 15, 17, 58, 38, 682, DateTimeKind.Utc), 3, "Popsuty głośnik", 4, 5 },
+                    { 4, new DateTime(2018, 12, 15, 17, 58, 38, 682, DateTimeKind.Utc), 3, "Klientowi nie działa klawiatura", 3, 4 },
+                    { 3, new DateTime(2018, 12, 15, 17, 58, 38, 682, DateTimeKind.Utc), 2, "Klient nie może dodzwonić się do nikogo - nie opłacił abonamentu", 2, 3 },
+                    { 7, new DateTime(2018, 12, 15, 17, 58, 38, 682, DateTimeKind.Utc), 5, "Coś nie diała", 2, 2 },
+                    { 2, new DateTime(2018, 12, 15, 17, 58, 38, 682, DateTimeKind.Utc), 1, "Opis z produktu dodamu tutaj", 5, 2 },
+                    { 1, new DateTime(2018, 12, 15, 17, 58, 38, 682, DateTimeKind.Utc), 1, "Tutaj powinien być jakiś opis naprawy", 1, 1 },
+                    { 8, new DateTime(2018, 12, 15, 17, 58, 38, 682, DateTimeKind.Utc), 5, "Pan nie był zadowolony", 5, 6 },
+                    { 6, new DateTime(2018, 12, 15, 17, 58, 38, 682, DateTimeKind.Utc), 4, "Klient przyniusł zalany telefon w skarpecie z ryżem", 1, 6 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "RepairItems",
+                columns: new[] { "RepairId", "SaparePartId", "Quantity" },
+                values: new object[,]
+                {
+                    { 1, 1, 1 },
+                    { 1, 2, 2 },
+                    { 2, 3, 1 },
+                    { 2, 2, 2 },
+                    { 2, 1, 1 },
+                    { 3, 4, 2 },
+                    { 3, 3, 3 },
+                    { 4, 5, 1 },
+                    { 4, 4, 1 },
+                    { 5, 1, 1 },
+                    { 5, 5, 10 }
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RepairItems_Repairs_RepairId",
+                table: "RepairItems",
+                column: "RepairId",
+                principalTable: "Repairs",
+                principalColumn: "RepairId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RepairItems_SapareParts_SaparePartId",
+                table: "RepairItems",
+                column: "SaparePartId",
+                principalTable: "SapareParts",
+                principalColumn: "SaparePartId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RepairItems_Repairs_RepairId",
+                table: "RepairItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RepairItems_SapareParts_SaparePartId",
+                table: "RepairItems");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RepairItems",
+                table: "RepairItems");
+
+            migrationBuilder.DeleteData(
+                table: "CustomerAddres",
+                keyColumn: "CustomerAddresId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "CustomerAddres",
+                keyColumn: "CustomerAddresId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "CustomerAddres",
+                keyColumn: "CustomerAddresId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "CustomerAddres",
+                keyColumn: "CustomerAddresId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "CustomerAddres",
+                keyColumn: "CustomerAddresId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "ProductSapareParts",
+                keyColumns: new[] { "ProductId", "SaparePartId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductSapareParts",
+                keyColumns: new[] { "ProductId", "SaparePartId" },
+                keyValues: new object[] { 1, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductSapareParts",
+                keyColumns: new[] { "ProductId", "SaparePartId" },
+                keyValues: new object[] { 2, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductSapareParts",
+                keyColumns: new[] { "ProductId", "SaparePartId" },
+                keyValues: new object[] { 2, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductSapareParts",
+                keyColumns: new[] { "ProductId", "SaparePartId" },
+                keyValues: new object[] { 3, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductSapareParts",
+                keyColumns: new[] { "ProductId", "SaparePartId" },
+                keyValues: new object[] { 3, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductSapareParts",
+                keyColumns: new[] { "ProductId", "SaparePartId" },
+                keyValues: new object[] { 4, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductSapareParts",
+                keyColumns: new[] { "ProductId", "SaparePartId" },
+                keyValues: new object[] { 4, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductSapareParts",
+                keyColumns: new[] { "ProductId", "SaparePartId" },
+                keyValues: new object[] { 5, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductSapareParts",
+                keyColumns: new[] { "ProductId", "SaparePartId" },
+                keyValues: new object[] { 5, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "RepairItems",
+                keyColumns: new[] { "RepairId", "SaparePartId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "RepairItems",
+                keyColumns: new[] { "RepairId", "SaparePartId" },
+                keyValues: new object[] { 1, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "RepairItems",
+                keyColumns: new[] { "RepairId", "SaparePartId" },
+                keyValues: new object[] { 2, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "RepairItems",
+                keyColumns: new[] { "RepairId", "SaparePartId" },
+                keyValues: new object[] { 2, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "RepairItems",
+                keyColumns: new[] { "RepairId", "SaparePartId" },
+                keyValues: new object[] { 2, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "RepairItems",
+                keyColumns: new[] { "RepairId", "SaparePartId" },
+                keyValues: new object[] { 3, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "RepairItems",
+                keyColumns: new[] { "RepairId", "SaparePartId" },
+                keyValues: new object[] { 3, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "RepairItems",
+                keyColumns: new[] { "RepairId", "SaparePartId" },
+                keyValues: new object[] { 4, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "RepairItems",
+                keyColumns: new[] { "RepairId", "SaparePartId" },
+                keyValues: new object[] { 4, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "RepairItems",
+                keyColumns: new[] { "RepairId", "SaparePartId" },
+                keyValues: new object[] { 5, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "RepairItems",
+                keyColumns: new[] { "RepairId", "SaparePartId" },
+                keyValues: new object[] { 5, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "Repairs",
+                keyColumn: "RepairId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Repairs",
+                keyColumn: "RepairId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Repairs",
+                keyColumn: "RepairId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "CustomerId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "CustomerId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "RepairStatuses",
+                keyColumn: "RepairStatusId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Repairs",
+                keyColumn: "RepairId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Repairs",
+                keyColumn: "RepairId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Repairs",
+                keyColumn: "RepairId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Repairs",
+                keyColumn: "RepairId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Repairs",
+                keyColumn: "RepairId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "SapareParts",
+                keyColumn: "SaparePartId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "SapareParts",
+                keyColumn: "SaparePartId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "SapareParts",
+                keyColumn: "SaparePartId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "SapareParts",
+                keyColumn: "SaparePartId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "SapareParts",
+                keyColumn: "SaparePartId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "CustomerId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "CustomerId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "CustomerId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "RepairStatuses",
+                keyColumn: "RepairStatusId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "RepairStatuses",
+                keyColumn: "RepairStatusId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "RepairStatuses",
+                keyColumn: "RepairStatusId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "RepairStatuses",
+                keyColumn: "RepairStatusId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "RepairStatuses",
+                keyColumn: "RepairStatusId",
+                keyValue: 5);
+
+            migrationBuilder.DropColumn(
+                name: "CreateDate",
+                table: "Repairs");
+
+            migrationBuilder.RenameTable(
+                name: "RepairItems",
+                newName: "SaparePartItems");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RepairItems_SaparePartId",
+                table: "SaparePartItems",
+                newName: "IX_SaparePartItems_SaparePartId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SaparePartItems",
+                table: "SaparePartItems",
+                columns: new[] { "RepairId", "SaparePartId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SaparePartItems_Repairs_RepairId",
+                table: "SaparePartItems",
+                column: "RepairId",
+                principalTable: "Repairs",
+                principalColumn: "RepairId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SaparePartItems_SapareParts_SaparePartId",
+                table: "SaparePartItems",
+                column: "SaparePartId",
+                principalTable: "SapareParts",
+                principalColumn: "SaparePartId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
