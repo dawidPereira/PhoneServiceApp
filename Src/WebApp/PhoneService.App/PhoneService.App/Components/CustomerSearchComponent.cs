2e425adce4e8d6db547e825e0ad7714906f6e622
Maksymilian Matuła <maksymilian.matula@gmail.com>
2018-12-31 13:25:27 +0100

    search customer

---
+++ Src/WebApp/PhoneService.App/PhoneService.App/Components/CustomerSearchComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PhoneService.Core.Models.Customer;
+
+namespace PhoneService.App.Components
+{
+    public class CustomerSearchComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var customerSearch = new CustomerSearchRequest();
+
+            return View("CustomerSearch", customerSearch);
+        }
+    }
+}
