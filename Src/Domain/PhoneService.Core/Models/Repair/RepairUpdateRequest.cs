a16362d12b90c75a3bbded4ff605cfb4c8cee2f6
Dawid Pereira <pereiradawid@outlook.com>
2019-01-04 19:16:18 +0100

    Add Validation

--- Src/Domain/PhoneService.Core/Models/Repair/RepairUpdateRequest.cs
+++ Src/Domain/PhoneService.Core/Models/Repair/RepairUpdateRequest.cs
@@ -5,17 +5,26 @@ using System.Collections.Generic;
 using System.Text;
 using PhoneService.Core.Models.Customer;
 using PhoneService.Core.Models.Healpers;
+using System.ComponentModel.DataAnnotations;
 
 namespace PhoneService.Core.Models.Repair
 {
     public class RepairUpdateRequest
     {
+        [Required(ErrorMessage = "Id naprawy jest wymagane")]
         public int RepairId { get; set; }
+
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "Id statusu jest wymagane")]
         public int StatusId { get; set; }
 
+        [Required(ErrorMessage = "Id klienta jest wymagane")]
         public int CustomerId { get; set; }
+
+        [Required(ErrorMessage = "Id produktu jest wymagane")]
         public int ProductId { get; set; }
+
         public CustomerDecisionLink Links { get; set; }
 
         public List<RepairItemAddRequest> RepairItems { get; set; }
--- Src/Domain/PhoneService.Core/Models/SaparePart/SaparePartAddRequest.cs
+++ Src/Domain/PhoneService.Core/Models/SaparePart/SaparePartAddRequest.cs
@@ -1,14 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace PhoneService.Core.Models.SaparePart
 {
     public class SaparePartAddRequest
     {
+        [Required(ErrorMessage = "Nazwa jest wymagana")]
+        [StringLength(25, ErrorMessage = "Nazwa może mieć maksymalnie 25 znaków")]
         public string Name { get; set; }
+
+        [StringLength(50, ErrorMessage = "Numer Referencyjny może mieć maksymalnie 50 znaków")]
         public string ReferenceNumebr { get; set; }
+
+        [Required(ErrorMessage = "Cena jest wymagana")]
+        [Range(1 , 99999, ErrorMessage ="Cena musi zawierać się w przedziale od 0 do 99999 zł")]
         public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Id produktu jest wymagane")]
         public int ProductId { get; set; }
     }
 }
