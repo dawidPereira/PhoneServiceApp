95c00cf9aa7353032c0503d2cb652f69ad8b2059
Maksymilian Matuła <maksymilian.matula@gmail.com>
2019-01-06 21:44:20 +0100

    fix you can't add two same parts to one repair

--- Src/WebApp/PhoneService.App/PhoneService.App/Components/RepairForSaparePartSelectComponent.cs
+++ Src/WebApp/PhoneService.App/PhoneService.App/Components/RepairForSaparePartSelectComponent.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using PhoneService.Core.Interfaces;
 using PhoneService.Core.Models.Repair;
 
@@ -21,7 +23,26 @@ namespace PhoneService.App.Components
         {
             var sapareParts = await _saparePartService.GetAllSaparePartByProductIdAsync(productId);
 
-            return View("RepairForSaparePartSelect", sapareParts);
+            var partsToDisplay = sapareParts.ToList();
+
+            var str = HttpContext.Session.GetString("repairUpdateRequest");
+
+            if (str != null)
+            {
+                var data = JsonConvert.DeserializeObject<RepairUpdateRequest>(str);
+
+                var items = data.RepairItems.Select(x => x);
+
+                if (items.Any())
+                {
+                    foreach (var item in items)
+                    {
+                        partsToDisplay.RemoveAll(x => x.SaparePartId == item.SaparePartId);
+                    }
+                }
+            }
+
+            return View("RepairForSaparePartSelect", partsToDisplay);
         }
     }
 }
