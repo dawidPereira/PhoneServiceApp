6e82142090b8a48feb30cbe2c6224907386d4a8a
Dawid Pereira <pereiradawid@outlook.com>
2019-01-09 11:48:42 +0100

    Add seed data

--- Src/Infrastructure/PhoneService.Persistance/PhoneServiceDbContext.cs
+++ Src/Infrastructure/PhoneService.Persistance/PhoneServiceDbContext.cs
@@ -89,46 +89,121 @@ namespace PhoneService.Persistance
             #region Seed Data
 
             modelBuilder.Entity<Customer>().HasData(
-                new Customer { CustomerId = 1, Email = "pereiradawid@example.com", Name = "Dawid", LastName = "Pereira", PhoneNum = "123456789", TaxNum = "987654321" },
-                new Customer { CustomerId = 2, Email = "maksymilainmatula@example.com", Name = "Maksymilian", LastName = "Matuła", PhoneNum = "123456789", TaxNum = "987654321" },
-                new Customer { CustomerId = 3, Email = "miloszwinnicki@example.com", Name = "Miłosz", LastName = "Winnicki", PhoneNum = "123456789", TaxNum = "987654321" },
-                new Customer { CustomerId = 4, Email = "rafalpasek@example.com", Name = "Rafał", LastName = "Pasek", PhoneNum = "123456789", TaxNum = "987654321" },
-                new Customer { CustomerId = 5, Email = "grzegorzwojcik@example.com", Name = "Grzegorz", LastName = "Wójcik", PhoneNum = "123456789", TaxNum = "987654321" });
+                new Customer { CustomerId = 1, Email = "pereiradawid@example.com", Name = "Dawid", LastName = "Pereira", PhoneNum = "781 361 182", TaxNum = "91032298349" },
+                new Customer { CustomerId = 2, Email = "maksymilainmatula@example.com", Name = "Maksymilian", LastName = "Matuła", PhoneNum = "685 432 234", TaxNum = "89022338243" },
+                new Customer { CustomerId = 3, Email = "miloszwinnicki@example.com", Name = "Miłosz", LastName = "Winnicki", PhoneNum = "783 234 432", TaxNum = "88113029383" },
+                new Customer { CustomerId = 4, Email = "rafalpasek@example.com", Name = "Rafał", LastName = "Pasek", PhoneNum = "984 343 234", TaxNum = "94022829304" },
+                new Customer { CustomerId = 5, Email = "grzegorzwojcik@example.com", Name = "Grzegorz", LastName = "Wójcik", PhoneNum = "745 543 321", TaxNum = "90010129348" },
+                new Customer { CustomerId = 6, Email = "zbigniewwolski@example.com", Name = "Zbigniew", LastName = "Wolski", PhoneNum = "500 433 333", TaxNum = "70071429378" },
+                new Customer { CustomerId = 7, Email = "tomaszbrzyski@example.com", Name = "Tomasz", LastName = "Brzyski", PhoneNum = "678 432 342", TaxNum = "66052229304" },
+                new Customer { CustomerId = 8, Email = "monikazawadzka@example.com", Name = "Monika", LastName = "Zawadzka", PhoneNum = "787 438 282", TaxNum = "99011039456" },
+                new Customer { CustomerId = 9, Email = "zofiawlodarczyk@example.com", Name = "Zofia", LastName = "Włodarczyk", PhoneNum = "378 392 234", TaxNum = "51031993845" },
+                new Customer { CustomerId = 10, Email = "nataliabudzinska@example.com", Name = "Natalia", LastName = "Brudzińska", PhoneNum = "878 984 774", TaxNum = "87083095743" },
+                new Customer { CustomerId = 11, Email = "emanuelmazowiecki@example.com", Name = "Emanuel", LastName = "Mazowiecki", PhoneNum = "577 573 572", TaxNum = "98070574834" },
+                new Customer { CustomerId = 12, Email = "ewelinadobkowska@example.com", Name = "Ewelina", LastName = "Dobkowska", PhoneNum = "675 584 994", TaxNum = "65091138596" });
 
             modelBuilder.Entity<CustomerAddres>().HasData(
-                new CustomerAddres { CustomerAddresId = 1, City = "Przeworsk", PostCode = "37-200", CustomerId = 1 },
-                new CustomerAddres { CustomerAddresId = 2, City = "Dąbrowa Górnicza", PostCode = "43-204", CustomerId = 2 },
-                new CustomerAddres { CustomerAddresId = 3, City = "Rzeszów", PostCode = "35-356", CustomerId = 3 },
-                new CustomerAddres { CustomerAddresId = 4, City = "Sosnowiec", PostCode = "30-300", CustomerId = 4 },
-                new CustomerAddres { CustomerAddresId = 5, City = "Żwirki i Muchomorki", PostCode = "11-222", CustomerId = 5 });
+                new CustomerAddres { CustomerAddresId = 1, Adress = "Maćkówka 155", City = "Przeworsk", PostCode = "37-200", CustomerId = 1 },
+                new CustomerAddres { CustomerAddresId = 2, Adress = "Zawiła 12", City = "Dąbrowa Górnicza", PostCode = "43-204", CustomerId = 2 },
+                new CustomerAddres { CustomerAddresId = 3, Adress = "Przy Torze 3", City = "Rzeszów", PostCode = "35-356", CustomerId = 3 },
+                new CustomerAddres { CustomerAddresId = 4, Adress = "Zaczernie 23", City = "Sosnowiec", PostCode = "30-300", CustomerId = 4 },
+                new CustomerAddres { CustomerAddresId = 5, Adress = "1000-lecia 89", City = "Żwirki i Muchomorki", PostCode = "11-222", CustomerId = 5 },
+                new CustomerAddres { CustomerAddresId = 6, Adress = "Powstańców Warszawy 56", City = "Rzeszów", PostCode = "25-567", CustomerId = 6 },
+                new CustomerAddres { CustomerAddresId = 7, Adress = "Tadeusza Kościuszki 558", City = "Rzeszów", PostCode = "45 -432", CustomerId = 7 },
+                new CustomerAddres { CustomerAddresId = 8, Adress = "Wesoła 11", City = "Łańcut", PostCode = "94-675", CustomerId = 8 },
+                new CustomerAddres { CustomerAddresId = 9, Adress = "Zarzecze 87", City = "Tarnów", PostCode = "65-987", CustomerId = 9 },
+                new CustomerAddres { CustomerAddresId = 10, Adress = "Osiedle Spokojna Dolina 23", City = "Jarosław", PostCode = "45-342", CustomerId = 10 },
+                new CustomerAddres { CustomerAddresId = 11, Adress = "Kolorowa 93", City = "Przemyśl", PostCode = "43-432", CustomerId = 11 },
+                new CustomerAddres { CustomerAddresId = 12, Adress = "Podgórze 9", City = "Rzeszów", PostCode = "21-765", CustomerId = 12 });
 
             modelBuilder.Entity<Product>().HasData(
                 new Product { ProductId = 1, Producer = "Samsung", Model = "S8", Description = "Nie wiem po co nam opis produktu" },
                 new Product { ProductId = 2, Producer = "Nokia", Model = "3310", Description = "Mam wrażenie, że trzeba usunąć opis" },
-                new Product { ProductId = 3, Producer = "Xiaomi", Model = "Lepszy", Description = "Przepraszam czy to pomyłka" },
-                new Product { ProductId = 4, Producer = "Apple", Model = "XXX", Description = "Policja? - Proszę przyjechać na facebooka" },
-                new Product { ProductId = 5, Producer = "Sony Ericson", Model = "Ericson Sony", Description = "Co jam ma tu wpisać ?" });
+                new Product { ProductId = 3, Producer = "Xiaomi", Model = "Mi 8", Description = "Przepraszam czy to pomyłka" },
+                new Product { ProductId = 4, Producer = "Redmi", Model = "4A", Description = "Policja? - Proszę przyjechać na facebooka" },
+                new Product { ProductId = 5, Producer = "Huwawei", Model = "P20", Description = "Policja? - Proszę przyjechać na facebooka" },
+                new Product { ProductId = 6, Producer = "Sony", Model = "Xperia 10", Description = "Policja? - Proszę przyjechać na facebooka" },
+                new Product { ProductId = 7, Producer = "PocoPhone", Model = "X1", Description = "Policja? - Proszę przyjechać na facebooka" },
+                new Product { ProductId = 8, Producer = "HTC", Model = "Desire 10", Description = "Policja? - Proszę przyjechać na facebooka" },
+                new Product { ProductId = 9, Producer = "Apple", Model = "X", Description = "Policja? - Proszę przyjechać na facebooka" },
+                new Product { ProductId = 10, Producer = "Sony Ericson", Model = "SE6", Description = "Co jam ma tu wpisać ?" });
 
             modelBuilder.Entity<SaparePart>().HasData(
-                new SaparePart { SaparePartId = 1, Name = "Dioda W34", ReferenceNumber = "253FG32", Price = 12, },
-                new SaparePart { SaparePartId = 2, Name = "Tranzystor BX11", ReferenceNumber = "2352s32", Price = 13, },
-                new SaparePart { SaparePartId = 3, Name = "Wyświetlacz uniwersalny", ReferenceNumber = "23XX32", Price = 18, },
-                new SaparePart { SaparePartId = 4, Name = "O co to za śróbka", ReferenceNumber = "5DS352", Price = 16, },
-                new SaparePart { SaparePartId = 5, Name = "Klawiatura 3310", ReferenceNumber = "2344ty32", Price = 12, });
+                new SaparePart { SaparePartId = 1, Name = "Wyświetlacz 5,5 cala", ReferenceNumber = "253FG32", Price = 150, },
+                new SaparePart { SaparePartId = 2, Name = "Wyświetlacz 6,5 cala", ReferenceNumber = "2352s32", Price = 220, },
+                new SaparePart { SaparePartId = 3, Name = "Powłoka ochronna 5,5 cala", ReferenceNumber = "23XX32", Price = 18, },
+                new SaparePart { SaparePartId = 4, Name = "Powłoka ochronna 6,5 cala", ReferenceNumber = "5DS352", Price = 22, },
+                new SaparePart { SaparePartId = 5, Name = "Bateria 2200 mAh", ReferenceNumber = "5DS352", Price = 180, },
+                new SaparePart { SaparePartId = 6, Name = "Bateria 3000 mAh", ReferenceNumber = "5DS352", Price = 275, },
+                new SaparePart { SaparePartId = 7, Name = "Pokrowiec ochronny 5,5 cala", ReferenceNumber = "5DS352", Price = 29, },
+                new SaparePart { SaparePartId = 8, Name = "Pokrowiec ochronny 6,5 cala", ReferenceNumber = "5DS352", Price = 49, },
+                new SaparePart { SaparePartId = 9, Name = "Ładowarka", ReferenceNumber = "5DS352", Price = 49, },
+                new SaparePart { SaparePartId = 10, Name = "Spersonalizowane Etui", ReferenceNumber = "5DS352", Price = 149, });
 
             modelBuilder.Entity<ProductSaparePart>().HasData(
                 new ProductSaparePart { ProductId = 1, SaparePartId = 1 },
                 new ProductSaparePart { ProductId = 1, SaparePartId = 3 },
-                new ProductSaparePart { ProductId = 2, SaparePartId = 5 },
+                new ProductSaparePart { ProductId = 1, SaparePartId = 5 },
+                new ProductSaparePart { ProductId = 1, SaparePartId = 7 },
+                new ProductSaparePart { ProductId = 1, SaparePartId = 9 },
+                new ProductSaparePart { ProductId = 1, SaparePartId = 10 },
                 new ProductSaparePart { ProductId = 2, SaparePartId = 2 },
-                new ProductSaparePart { ProductId = 3, SaparePartId = 3 },
+                new ProductSaparePart { ProductId = 2, SaparePartId = 4 },
+                new ProductSaparePart { ProductId = 2, SaparePartId = 6 },
+                new ProductSaparePart { ProductId = 2, SaparePartId = 8 },
+                new ProductSaparePart { ProductId = 2, SaparePartId = 9 },
+                new ProductSaparePart { ProductId = 2, SaparePartId = 10 },
+                new ProductSaparePart { ProductId = 3, SaparePartId = 2 },
                 new ProductSaparePart { ProductId = 3, SaparePartId = 4 },
-                new ProductSaparePart { ProductId = 4, SaparePartId = 1 },
+                new ProductSaparePart { ProductId = 3, SaparePartId = 6 },
+                new ProductSaparePart { ProductId = 3, SaparePartId = 8 },
+                new ProductSaparePart { ProductId = 3, SaparePartId = 9 },
+                new ProductSaparePart { ProductId = 3, SaparePartId = 10 },
                 new ProductSaparePart { ProductId = 4, SaparePartId = 2 },
-                new ProductSaparePart { ProductId = 5, SaparePartId = 4 },
-                new ProductSaparePart { ProductId = 5, SaparePartId = 3 });
+                new ProductSaparePart { ProductId = 4, SaparePartId = 4 },
+                new ProductSaparePart { ProductId = 4, SaparePartId = 6 },
+                new ProductSaparePart { ProductId = 4, SaparePartId = 8 },
+                new ProductSaparePart { ProductId = 4, SaparePartId = 9 },
+                new ProductSaparePart { ProductId = 4, SaparePartId = 10 },
+                new ProductSaparePart { ProductId = 5, SaparePartId = 1 },
+                new ProductSaparePart { ProductId = 5, SaparePartId = 3 },
+                new ProductSaparePart { ProductId = 5, SaparePartId = 5 },
+                new ProductSaparePart { ProductId = 5, SaparePartId = 7 },
+                new ProductSaparePart { ProductId = 5, SaparePartId = 9 },
+                new ProductSaparePart { ProductId = 5, SaparePartId = 10 },
+                new ProductSaparePart { ProductId = 6, SaparePartId = 2 },
+                new ProductSaparePart { ProductId = 6, SaparePartId = 4 },
+                new ProductSaparePart { ProductId = 6, SaparePartId = 6 },
+                new ProductSaparePart { ProductId = 6, SaparePartId = 8 },
+                new ProductSaparePart { ProductId = 6, SaparePartId = 9 },
+                new ProductSaparePart { ProductId = 6, SaparePartId = 10 },
+                new ProductSaparePart { ProductId = 7, SaparePartId = 1 },
+                new ProductSaparePart { ProductId = 7, SaparePartId = 3 },
+                new ProductSaparePart { ProductId = 7, SaparePartId = 5 },
+                new ProductSaparePart { ProductId = 7, SaparePartId = 7 },
+                new ProductSaparePart { ProductId = 7, SaparePartId = 9 },
+                new ProductSaparePart { ProductId = 7, SaparePartId = 10 },
+                new ProductSaparePart { ProductId = 8, SaparePartId = 2 },
+                new ProductSaparePart { ProductId = 8, SaparePartId = 4 },
+                new ProductSaparePart { ProductId = 8, SaparePartId = 6 },
+                new ProductSaparePart { ProductId = 8, SaparePartId = 8 },
+                new ProductSaparePart { ProductId = 8, SaparePartId = 9 },
+                new ProductSaparePart { ProductId = 8, SaparePartId = 10 },
+                new ProductSaparePart { ProductId = 9, SaparePartId = 1 },
+                new ProductSaparePart { ProductId = 9, SaparePartId = 3 },
+                new ProductSaparePart { ProductId = 9, SaparePartId = 5 },
+                new ProductSaparePart { ProductId = 9, SaparePartId = 7 },
+                new ProductSaparePart { ProductId = 9, SaparePartId = 9 },
+                new ProductSaparePart { ProductId = 9, SaparePartId = 10 },
+                new ProductSaparePart { ProductId = 10, SaparePartId = 2 },
+                new ProductSaparePart { ProductId = 10, SaparePartId = 4 },
+                new ProductSaparePart { ProductId = 10, SaparePartId = 6 },
+                new ProductSaparePart { ProductId = 10, SaparePartId = 8 },
+                new ProductSaparePart { ProductId = 10, SaparePartId = 9 },
+                new ProductSaparePart { ProductId = 10, SaparePartId = 10 });
 
             modelBuilder.Entity<RepairStatus>().HasData(
+
                 new RepairStatus { RepairStatusId = 1, Name = "Przyjeta" },
                 new RepairStatus { RepairStatusId = 2, Name = "Wyceniona" },
                 new RepairStatus { RepairStatusId = 3, Name = "W trakcie realizacji" },
@@ -138,14 +213,14 @@ namespace PhoneService.Persistance
                 new RepairStatus { RepairStatusId = 7, Name = "Zakończona bez naprawy" });
 
             modelBuilder.Entity<Repair>().HasData(
-                new Repair { RepairId = 1, CustomerId = 1, ProductId = 1, RepairStatusId = 1, CreateDate = DateTime.UtcNow, Description = "Tutaj powinien być jakiś opis naprawy" },
-                new Repair { RepairId = 2, CustomerId = 1, ProductId = 5, RepairStatusId = 2, CreateDate = DateTime.UtcNow, Description = "Opis z produktu dodamu tutaj" },
-                new Repair { RepairId = 3, CustomerId = 2, ProductId = 2, RepairStatusId = 3, CreateDate = DateTime.UtcNow, Description = "Klient nie może dodzwonić się do nikogo - nie opłacił abonamentu" },
-                new Repair { RepairId = 4, CustomerId = 3, ProductId = 3, RepairStatusId = 4, CreateDate = DateTime.UtcNow, Description = "Klientowi nie działa klawiatura" },
-                new Repair { RepairId = 5, CustomerId = 3, ProductId = 4, RepairStatusId = 5, CreateDate = DateTime.UtcNow, Description = "Popsuty głośnik" },
-                new Repair { RepairId = 6, CustomerId = 4, ProductId = 1, RepairStatusId = 6, CreateDate = DateTime.UtcNow, Description = "Klient przyniusł zalany telefon w skarpecie z ryżem" },
-                new Repair { RepairId = 7, CustomerId = 5, ProductId = 2, RepairStatusId = 2, CreateDate = DateTime.UtcNow, Description = "Coś nie diała" },
-                new Repair { RepairId = 8, CustomerId = 5, ProductId = 5, RepairStatusId = 6, CreateDate = DateTime.UtcNow, Description = "Pan nie był zadowolony" });
+                new Repair { RepairId = 1, CustomerId = 1, ProductId = 1, RepairStatusId = 1, CreateDate = DateTime.UtcNow, Description = "Klient zgłasza problem z krótkim czasem pracy telefonu" },
+                new Repair { RepairId = 2, CustomerId = 1, ProductId = 5, RepairStatusId = 1, CreateDate = DateTime.UtcNow, Description = "Po aktualizacji systemu, telefon znacznie spowolnił. Pojawiają się błędy graficzne, a telefon się przegrzewa" },
+                new Repair { RepairId = 3, CustomerId = 2, ProductId = 2, RepairStatusId = 1, CreateDate = DateTime.UtcNow, Description = "Klient nie może dodzwonić się do nikogo" },
+                new Repair { RepairId = 4, CustomerId = 3, ProductId = 3, RepairStatusId = 1, CreateDate = DateTime.UtcNow, Description = "Klientowi nie działa klawiatura" },
+                new Repair { RepairId = 5, CustomerId = 3, ProductId = 4, RepairStatusId = 1, CreateDate = DateTime.UtcNow, Description = "Popsuty głośnik" },
+                new Repair { RepairId = 6, CustomerId = 4, ProductId = 1, RepairStatusId = 1, CreateDate = DateTime.UtcNow, Description = "Telefon został zalany przez klienta. Po zdarzeniu przestał działać wyświetlacz oraz głośnik" },
+                new Repair { RepairId = 7, CustomerId = 5, ProductId = 2, RepairStatusId = 1, CreateDate = DateTime.UtcNow, Description = "Klient serwisował telefon u konkurencji, naprawa nie odniosła pozytynego skutku. Zgłosił sie do nas z prośba o rozwiązanie problemu z niedziałajacą baterią, oraz z prosbą o personalizację etiu." },
+                new Repair { RepairId = 8, CustomerId = 5, ProductId = 5, RepairStatusId = 1, CreateDate = DateTime.UtcNow, Description = "Klient prosi o szybki czas realizacji" });
 
             modelBuilder.Entity<EmailSubject>().HasData(
                 new EmailSubject { EmailSubjectId = 1, Subject = "Twoja naprawa została wyceniona" },
